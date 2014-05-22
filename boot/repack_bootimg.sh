927194dbe0b911c19b122447b0e6f9b918fc16f6
Soul Trace <S-trace@list.ru>
2014-05-22 05:14:06 +0400

    Final fix

--- boot/repack_bootimg.sh
+++ boot/repack_bootimg.sh
@@ -5,7 +5,7 @@ cd ramdisk
 sudo find| sudo cpio -H newc -o | gzip -9 > ../ramdisk_$build.cpio.gz
 cd ..
 sudo chown s-trace:s-trace -R ramdisk
-./mkbootimg --kernel boot.img-zImage --ramdisk ramdisk_CM11.gz --base 0x80008000 --pagesize 2048 -o boot_$build.img
+./mkbootimg --kernel boot.img-zImage --ramdisk ramdisk_$build.cpio.gz --base 0x80008000 --pagesize 2048 -o boot_$build.img
 echo boot_$build.img built
 let build_next=build+1
 echo $build_next > build
