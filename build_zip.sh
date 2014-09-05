49ee31704cc8242e501402b21023d3cdd2b4a2bd
Soul Trace <S-trace@list.ru>
2014-09-05 17:31:38 +0400

    Change default output name for ZIP

--- build_zip.sh
+++ build_zip.sh
@@ -37,18 +37,18 @@ echo $build_next > build
 rm -rf out/
 mkdir -p out/
 
-zip out/E8_3D-cm11.0-compatibility-rc1_rev${build}.zip META-INF system specific build_prop_patcher.sh boot_*.img -r
-echo out/E8_3D-cm11.0-compatibility-rc1_rev${build}.zip built
-zip out/E8_3D-cm11.0-compatibility-rc1_rev${build}_helpers.zip aml_autoscript Init_FS_after_NAND_scrub.zip u-boot.bin
-echo out/E8_3D-cm11.0-compatibility-rc1_rev${build}_helpers.zip built
+zip out/E8_3D-KitKat-compatibility-rc1_rev${build}.zip META-INF system specific build_prop_patcher.sh boot_*.img -r
+echo out/E8_3D-KitKat-compatibility-rc1_rev${build}.zip built
+zip out/E8_3D-KitKat-compatibility-rc1_rev${build}_helpers.zip aml_autoscript Init_FS_after_NAND_scrub.zip u-boot.bin
+echo out/E8_3D-KitKat-compatibility-rc1_rev${build}_helpers.zip built
 
 for file in recovery_*.img ; do 
   zip out/$file.zip $file
   echo out/$file.zip built
 done
 
-until adb push out/E8_3D-cm11.0-compatibility-rc1_rev${build}.zip /sdcard/; do sleep 1 ; done
-#until adb push out/E8_3D-cm11.0-compatibility-rc1_rev${build}.zip /media/; do sleep 1 ; done
+until adb push out/E8_3D-KitKat-compatibility-rc1_rev${build}.zip /sdcard/; do sleep 1 ; done
+#until adb push out/E8_3D-KitKat-compatibility-rc1_rev${build}.zip /media/; do sleep 1 ; done
 # until adb push boot.img /tmp/; do sleep 1 ; done
 # until adb shell flash_image boot /tmp/boot.img; do sleep 1 ; done
 echo Compat ${build} built
